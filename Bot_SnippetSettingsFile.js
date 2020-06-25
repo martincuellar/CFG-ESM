03caedebb532e6931f8d8577fbe29eb79510b33b
Martin Cuéllar <martin@mcuellar.com>
2020-06-25 08:55:05 -0500

    Add files via upload

---
+++ Bot_SnippetSettingsFile.js
@@ -0,0 +1,19 @@
+window._snapinsSnippetSettingsFile = (function() {
+
+  // Logs that the snippet settings file was loaded successfully
+  //console.log("Snippet settings file loaded.");
+
+  embedded_svc.snippetSettingsFile.extraPrechatFormDetails = [{
+    "label":"First Name", 
+    "transcriptFields": ["cfg_First_Name__c"]
+  },{
+    "label":"Last Name", 
+    "transcriptFields": ["cfg_Last_Name__c"]
+  },{
+    "label":"Email", 
+    "transcriptFields": ["cfg_Email__c"]
+  },{
+    "label":"Subject", 
+    "transcriptFields": ["cfg_Subject__c"]
+  }];
+})();
